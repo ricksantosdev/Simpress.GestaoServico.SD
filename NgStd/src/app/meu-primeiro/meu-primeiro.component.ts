d302a5ba8efb970861bad3eecf8b96b942f8027e
Ricardo Santos <ri.santos@hotmail.com>
2018-12-14 08:53:30 -0200

    Aula - Criação de Componente sem / com Angular CLI

---
+++ NgStd/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'; //importando funções
+
+@Component({
+  selector: 'meu-primeiro-component',
+  template: `
+            <p>  Meu primeiro component </p>
+            `
+}) //decorando a classe
+export class MeuPrimeiroComponent {
+
+}
